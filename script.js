4af4c75acc9945b842f036f0463c20e323982d4e
dlhardi <derricklhardison@gmail.com>
2021-04-13 00:30:04 -0400

    Refactored code by creating functions and calling them on button clicks instead of using anonymous functions on the event listeners

--- script.js
+++ script.js
@@ -5,15 +5,20 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener("click", function () {
-    console.log("click");
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
-}
+const openModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
 
-btnCloseModal.addEventListener("click", function () {
+const closeModal = () => {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
-});
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
